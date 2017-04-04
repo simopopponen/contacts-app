4d5dac864308a551bfd0570cfce86d12ee488666
simopopponen <simo.popponen@student.saimia.fi>
2017-04-04 14:09:54 +0300

    Added necessary files to client side without code

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactListItemComponent } from './contact-list/contact-list-item/contact-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactListItemComponent
   ],
   imports: [
     BrowserModule,
