8cee3cefb754819bc7f10eeb25f7fbedf996dfff
simopopponen <simo.popponen@student.saimia.fi>
2017-04-19 12:36:14 +0300

    Working version of login screen

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,58 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {Contact} from "./contact";
-import {ContactService} from "./service/contact.service";
-import {DialogService} from "./service/dialog.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ContactService]
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
 
-  title = 'Contacts App';
-
-  contacts: Contact[];
-
-  constructor(private contactService: ContactService, private dialogService: DialogService) {}
-  // constructor(public dialogService: DialogService, public contactService: ContactService) {}
-
-  addContact(): void {
-    let input = this.dialogService.contactDialog();
-    input.subscribe(result => {
-      if (result) {
-        this.contactService.addContact(result);
-        this.loadContacts();
-      }
-    });
-  }
-  editContact(contact: Contact): void {
-    let input = this.dialogService.contactDialog(contact);
-    input.subscribe(result => {
-      if (result) {
-        this.contactService.updateContact(result);
-        this.loadContacts();
-      }
-    });
-  }
-
-  contactMap(contact: Contact){
-    let address = contact.streetAddress + ', ' + contact.postalCode + ' ' + contact.city;
-    this.dialogService.mapDialog(address);
-  }
-
-  removeContact(contact: Contact): void {
-    this.contactService.removeContact(contact.id.toString());
-    this.loadContacts();
-  }
-
-  loadContacts() {
-    this.contactService.getContacts().then(contacts => this.contacts = contacts);
-  }
-  ngOnInit(): void {
-    this.loadContacts();
-  }
-
-  // Call functions here for add, get and delete contacts
 }
--- client/src/app/contact/contact.component.ts
+++ client/src/app/contact/contact.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import {Contact} from "../contact";
+import {ContactService} from "../service/contact.service";
+import {DialogService} from "../service/dialog.service";
+
 
 @Component({
   selector: 'app-contact',
@@ -8,9 +12,46 @@ import {Router} from "@angular/router";
 })
 export class ContactComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  title = 'Contacts App';
 
-  ngOnInit() {
+  contacts: Contact[];
+
+  constructor(private router: Router, private contactService: ContactService, private dialogService: DialogService) {
   }
 
+  addContact(): void {
+    let input = this.dialogService.contactDialog();
+    input.subscribe(result => {
+      if (result) {
+        this.contactService.addContact(result);
+        this.loadContacts();
+      }
+    });
+  }
+  editContact(contact: Contact): void {
+    let input = this.dialogService.contactDialog(contact);
+    input.subscribe(result => {
+      if (result) {
+        this.contactService.updateContact(result);
+        this.loadContacts();
+      }
+    });
+  }
+
+  contactMap(contact: Contact){
+    let address = contact.streetAddress + ', ' + contact.postalCode + ' ' + contact.city;
+    this.dialogService.mapDialog(address);
+  }
+
+  removeContact(contact: Contact): void {
+    this.contactService.removeContact(contact.id.toString());
+    this.loadContacts();
+  }
+
+  loadContacts() {
+    this.contactService.getContacts().then(contacts => this.contacts = contacts);
+  }
+  ngOnInit(): void {
+    this.loadContacts();
+  }
 }
